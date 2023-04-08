d0e4674afa0e9fd982bc45c21bf6c2f216b4651f
Victor <vitek.borodyn@gmail.com>
2023-04-08 03:47:08 +0300

    Add calculation of the positioning option

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { Coordinates } from "./type/coordinates.type";
 import { ShippingDto } from "./dto/shipping.dto";
+import { Positioning } from "./type/positioning.type";
 
 @Injectable()
 export class AppService {
@@ -13,17 +14,30 @@ export class AppService {
     iterationNumber,
     customers,
   }: ShippingDto): Coordinates {
-    let x = 0,
-      y = 0;
+    const positionings: Positioning[] = Array.from(
+      { length: iterationNumber },
+      () => ({
+        coordinates: { x: 0, y: 0 },
+        costs: 0,
+      })
+    );
 
-    for (let i = 0; i < iterationNumber; i++) {
+    positionings.forEach((positioning, index) => {
+      const previousIndex = index === 0 ? index : index - 1;
+
       const { xDividend, yDividend, divisor, transportCosts } =
         customers.reduce(
           (acc, { coordinates, transportTariff, productVolume }) => {
             const multiplier =
               (authorizedCapital * transportTariff) /
               (this.getPresentCosts(authorizedCapital, productVolume) *
-                Math.sqrt((coordinates.x - x) ** 2 + (coordinates.y - y) ** 2));
+                Math.sqrt(
+                  (coordinates.x - positionings[previousIndex].coordinates.x) **
+                    2 +
+                    (coordinates.y -
+                      positionings[previousIndex].coordinates.y) **
+                      2
+                ));
 
             acc.xDividend += coordinates.x * multiplier;
             acc.yDividend += coordinates.y * multiplier;
@@ -35,9 +49,15 @@ export class AppService {
           { xDividend: 0, yDividend: 0, divisor: 0, transportCosts: 0 }
         );
 
-      x = xDividend / divisor;
-      y = yDividend / divisor;
-    }
-    return { x, y };
+      positioning.coordinates.x = xDividend / divisor;
+      positioning.coordinates.y = yDividend / divisor;
+      positioning.costs = transportCosts;
+    });
+
+    const minCostsPositioning = positionings.reduce((current, previous) =>
+      current.costs < previous.costs ? current : previous
+    );
+
+    return minCostsPositioning.coordinates;
   }
 }
---
+++ src/type/positioning.type.ts
@@ -0,0 +1,6 @@
+import { Coordinates } from "./coordinates.type";
+
+export type Positioning = {
+  coordinates: Coordinates;
+  costs: number;
+};
