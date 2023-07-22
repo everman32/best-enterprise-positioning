33fd50f0ae6c632a6731a523612d6d4fb24ee6a2
Victor <vitek.borodyn@gmail.com>
2023-07-22 23:37:38 +0300

    Add tests to all methods of the customer module

--- src/customer/customer.service.spec.ts
+++ src/customer/customer.service.spec.ts
@@ -7,7 +7,16 @@ describe("CustomerService", () => {
 
   const AUTHORIZED_CAPITAL = 5.5;
   const PRODUCT_VOLUME = 2.1;
-  const COSTS = 2.6194;
+  const COSTS = 2.619;
+
+  const TRANSPORT_TARIFF = 3.2;
+  const MULTIPLIER = 1.221;
+
+  const FIRST_POINT = { longitude: 0.55123, latitude: 0.72616 };
+  const SECOND_POINT = { longitude: 0.99847, latitude: 0.92154 };
+  const TRANSPORT_COSTS = 364.656;
+
+  const PRECISION = 2;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,9 +30,30 @@ describe("CustomerService", () => {
     expect(customerService).toBeDefined();
   });
 
-  it(`should return ${COSTS}`, () => {
+  it(`getPresentCosts should return ${COSTS}`, () => {
     expect(
       customerService.getPresentCosts(AUTHORIZED_CAPITAL, PRODUCT_VOLUME)
-    ).toBeCloseTo(COSTS, 3);
+    ).toBeCloseTo(COSTS, PRECISION);
+  });
+
+  it(`getMultiplier should return ${MULTIPLIER}`, () => {
+    expect(
+      customerService.getMultiplier(
+        AUTHORIZED_CAPITAL,
+        TRANSPORT_TARIFF,
+        PRODUCT_VOLUME
+      )
+    ).toBeCloseTo(MULTIPLIER, PRECISION);
+  });
+
+  it(`getTransportCosts should return ${TRANSPORT_COSTS}`, () => {
+    expect(
+      customerService.getTransportCosts(
+        PRODUCT_VOLUME,
+        TRANSPORT_TARIFF,
+        FIRST_POINT,
+        SECOND_POINT
+      )
+    ).toBeCloseTo(TRANSPORT_COSTS, PRECISION);
   });
 });
